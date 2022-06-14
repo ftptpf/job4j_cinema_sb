f7167864639cd84fc8d6fc9a05e744921cb968a6
ftptpf <inboxru@yandex.ru>
2022-06-14 09:46:59 +0300

    2. Сервис - Кинотеатр [#504869] - index Optional

--- src/main/java/ru/job4j/cinema/control/IndexController.java
+++ src/main/java/ru/job4j/cinema/control/IndexController.java
@@ -4,18 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.job4j.cinema.service.SessionService;
+import ru.job4j.cinema.service.TicketService;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
-    private final SessionService sessionService;
+    private final SessionService service;
+    private final TicketService ticketService;
 
-    public IndexController(SessionService sessionService) {
-        this.sessionService = sessionService;
+    public IndexController(SessionService service, TicketService ticketService) {
+        this.service = service;
+        this.ticketService = ticketService;
     }
 
     @GetMapping("/index")
     public String index(Model model) {
-        model.addAttribute("sessions", sessionService.findAll());
+        model.addAttribute("sessions", service.findAll());
+        Optional<Integer> idOptional = (Optional<Integer>) model.getAttribute("filmSession.id");
+        if (idOptional.isPresent()) {
+            int id = idOptional.get();
+            model.addAttribute("sessionTickets", ticketService.findAllSessionTickets(id));
+        }
 
         return "index";
     }
