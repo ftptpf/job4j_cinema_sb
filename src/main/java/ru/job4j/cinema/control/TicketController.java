a555d6e91fb7f83673109caad6b05827c9ee38fd
ftptpf <Nb49Rtdd>
2022-06-16 21:48:05 +0300

    2. Сервис - Кинотеатр [#504869] - Controller

--- src/main/java/ru/job4j/cinema/control/TicketController.java
+++ src/main/java/ru/job4j/cinema/control/TicketController.java
@@ -1,4 +1,13 @@
 package ru.job4j.cinema.control;
 
+import org.springframework.stereotype.Controller;
+import ru.job4j.cinema.service.TicketService;
+
+@Controller
 public class TicketController {
+    private final TicketService service;
+
+    public TicketController(TicketService service) {
+        this.service = service;
+    }
 }
--- src/main/java/ru/job4j/cinema/control/UserController.java
+++ src/main/java/ru/job4j/cinema/control/UserController.java
@@ -1,4 +1,13 @@
 package ru.job4j.cinema.control;
 
+import org.springframework.stereotype.Controller;
+import ru.job4j.cinema.service.UserService;
+
+@Controller
 public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
 }
