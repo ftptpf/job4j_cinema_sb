8258c509338eb71b912652037fb1ceb24460692c
ftptpf <inboxru@yandex.ru>
2022-06-16 09:49:08 +0300

    2. Сервис - Кинотеатр [#504869] - 4 steps

--- src/main/java/ru/job4j/cinema/control/IndexController.java
+++ src/main/java/ru/job4j/cinema/control/IndexController.java
@@ -4,29 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.job4j.cinema.service.SessionService;
-import ru.job4j.cinema.service.TicketService;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
     private final SessionService service;
-    private final TicketService ticketService;
 
-    public IndexController(SessionService service, TicketService ticketService) {
+    public IndexController(SessionService service) {
         this.service = service;
-        this.ticketService = ticketService;
     }
 
     @GetMapping("/index")
     public String index(Model model) {
         model.addAttribute("sessions", service.findAll());
-        Optional<Integer> idOptional = (Optional<Integer>) model.getAttribute("filmSession.id");
-        if (idOptional.isPresent()) {
-            int id = idOptional.get();
-            model.addAttribute("sessionTickets", ticketService.findAllSessionTickets(id));
-        }
-
         return "index";
     }
 }
--- src/main/java/ru/job4j/cinema/control/SessionController.java
+++ src/main/java/ru/job4j/cinema/control/SessionController.java
@@ -1,4 +1,27 @@
 package ru.job4j.cinema.control;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.job4j.cinema.model.Session;
+import ru.job4j.cinema.service.SessionService;
+
+@Controller
 public class SessionController {
+    private final SessionService service;
+
+    public SessionController(SessionService service) {
+        this.service = service;
+    }
+    @PostMapping("/session")
+    public String sessionId(@ModelAttribute Session session, Model model) {
+
+
+
+        return "tickets";
+
+
+    }
 }
